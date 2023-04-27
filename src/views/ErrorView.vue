f2c33fc042a69400439b230329727f9961078e19
空巷一人 <1530688385@qq.com>
2023-04-27 16:50:22 +0800

    修改404

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -2,8 +2,8 @@
   <div class="container rounded-md mx-auto bg-white flex items-center">
     <img class="w-[50%]" src="https://i.loli.net/2020/05/19/aKOcLiyPl2JQdFD.png" alt="" />
     <div class="w-[50%] text-center font-thin font-mono text-black">
-      <div class="title text-12xl">404</div>
-      <div class="text-3xl mt-5">Page Not Found</div>
+      <div class="title text-12xl <md:text-6xl">404</div>
+      <div class="text-3xl mt-5 <md:text-1xl">Page Not Found</div>
     </div>
   </div>
 </template>
